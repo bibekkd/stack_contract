4a8a3fac7562ba36cc5df61b45fb50341cf13788
Bibek Dey <bibek@Mac.lan>
2025-01-26 00:06:34 +0530

    stack and unstack function updated

--- src/StackContract.sol
+++ src/StackContract.sol
@@ -5,22 +5,23 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract StackContract is Ownable {
-    uint public amount;
-    address payable public stackingAddress;
     mapping (address => uint) stackBalances;
+    uint public totalStack;
 
     constructor() Ownable(msg.sender) {
-        stackingAddress = payable(msg.sender);
     }
 
-    function stack(uint _amount) public onlyOwner payable {
-        stackBalances[msg.sender] += _amount;
+    function stack() public onlyOwner payable {
+        require(msg.value > 0);
+        stackBalances[msg.sender] += msg.value;
+        totalStack += msg.value;
     }
 
     function unStack(uint _amount) public {
-        uint totalStackBalance = stackBalances[msg.sender];
-        require(totalStackBalance >= _amount);
-        uint remainingBalance = stackBalances[msg.sender] - _amount;
-        stackBalances[msg.sender] = remainingBalance;
+        uint ownerTotalStackedBalance = stackBalances[msg.sender];
+        require(ownerTotalStackedBalance >= _amount);
+        payable(msg.sender).transfer(_amount);
+        stackBalances[msg.sender] -= _amount;
+        totalStack -= _amount;
     }
 }
