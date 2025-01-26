94a60f0d7d436cf070d3dff398b92dfa761f94e1
Bibek Dey <bibek@Mac.lan>
2025-01-26 15:45:18 +0530

    Proxy contract added

--- src/StakeContract.sol
+++ src/StakeContract.sol
@@ -1,6 +1,18 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
+contract ProxyContract {
+    address public implementation;
+
+    constructor(address _implementation) {
+        implementation = _implementation;
+    }
+
+    fallback() external { 
+        (bool success, ) = implementation.delegatecall(msg.data);
+        require(success, "Delegatecall failed");
+    }
+}
 
 contract StakeContract {
     mapping (address => uint) stakeBalances;
